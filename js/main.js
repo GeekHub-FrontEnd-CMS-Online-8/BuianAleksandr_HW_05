932d3768600ed2910b3adcd690aa0e041ecd1afa
Alex_Bu <a.buyan@ukr.net>
2018-11-22 20:09:45 +0200

    -add hover to sotial - hooked up js to modal image preview

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+var showBigBtn = document.getElementsByClassName('show-big-prw');
+Array.from(showBigBtn).forEach(function (element) {
+  element.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    var imgSrc = this.parentNode.closest("li").childNodes[1].src;
+    var modal = document.getElementById('modal');
+    modal.style.display = 'block';
+    modal.style.backgroundImage = "url(".concat(imgSrc, ")");
+    console.log(imgSrc);
+  });
+});
+document.getElementById('modal-close').addEventListener('click', function (evt) {
+  document.getElementById('modal').style.display = 'none';
+});
